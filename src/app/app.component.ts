18468a586e571a8b4aa818a1b9c950bcaec2835d
Shubham Kumar <64249360+shubham-1903@users.noreply.github.com>
2023-10-10 22:41:57 +0530

    Update app.component.ts Title Changed 3rd time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Psl Test Shubham Kumar';
+  title = 'Psl Test Mahima Kumari';
 }
